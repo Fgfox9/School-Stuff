98c8edf6d614164aa5a5a9ea73e303259dd13c25
Emlyn <emlynphoenix1@gmail.com>
2023-03-06 22:04:43 +0000

    update 4

--- main.py
+++ main.py
@@ -1,59 +1,49 @@
-import tkinter as tk
-
-root = tk.Tk()
-root.title("Simple Calculator")
-
-e = tk.Entry(root, width=35, borderwidth=5)
-e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
-
-def button_click(number):
-    #e.delete(0, tk.END)
-    current = e.get()
-    e.delete(0, tk.END)
-    e.insert(0, str(current) + str(number))
-
-def button_clear():
-    e.delete(0, tk.END)
-
-def button_add():
-    return
+from tkinter import *
+from PIL import ImageTk, Image
 
 
-# Define Buttons
+root = Tk()
+root.title("Images")
 
-button_1 = tk.Button(root, text="1", padx=40, pady=20, command=lambda: button_click(1))
-button_2 = tk.Button(root, text="2", padx=40, pady=20, command=lambda: button_click(2))
-button_3 = tk.Button(root, text="3", padx=40, pady=20, command=lambda: button_click(3))
-button_4 = tk.Button(root, text="4", padx=40, pady=20, command=lambda: button_click(4))
-button_5 = tk.Button(root, text="5", padx=40, pady=20, command=lambda: button_click(5))
-button_6 = tk.Button(root, text="6", padx=40, pady=20, command=lambda: button_click(6))
-button_7 = tk.Button(root, text="7", padx=40, pady=20, command=lambda: button_click(7))
-button_8 = tk.Button(root, text="8", padx=40, pady=20, command=lambda: button_click(8))
-button_9 = tk.Button(root, text="9", padx=40, pady=20, command=lambda: button_click(9))
-button_0 = tk.Button(root, text="0", padx=40, pady=20, command=lambda: button_click(0))
+my_img1 = ImageTk.PhotoImage(Image.open("/Users/emlynphoenix/Downloads/Images/random.jpeg"))
+my_img2 = ImageTk.PhotoImage(Image.open("/Users/emlynphoenix/Downloads/Images/cast.png"))
+my_img3= ImageTk.PhotoImage(Image.open("/Users/emlynphoenix/Downloads//Images/Casty.png"))
 
-button_addition = tk.Button(root, text="+", padx=39, pady=20, command=lambda: button_add)
-button_equal = tk.Button(root, text="=", padx=91, pady=20, command=lambda: button_click)
-button_clear = tk.Button(root, text="Clear", padx=79, pady=20, command=button_clear)
+image_list = [my_img1, my_img2, my_img3]
 
-# Put the buttons on the screeen
+my_label = Label(image=my_img1)
+my_label.grid(row=0, column=0, columnspan=3)
 
-button_1.grid(row=3, column=0)
-button_2.grid(row=3, column=1)
-button_3.grid(row=3, column=2)
+def forward(image_number):
+    global my_label
+    global button_forward
+    global button_back
 
-button_4.grid(row=2, column=0) 
-button_5.grid(row=2, column=1)
-button_6.grid(row=2, column=2)
+    my_label.grid_forget()
+    my_label = Label(image=image_list[image_number-1])
+    button_forward = Button(root, text=">>", command=lambda: forward(image_number+1))
+    button_back = Button(root, text="<<", command=lambda: back(image_number-1))
+    my_label.grid(row=0, column=0, columnspan=3)
 
-button_7.grid(row=1, column=0)
-button_8.grid(row=1, column=1)
-button_9.grid(row=1, column=2)
+    button_back.grid(row=1, column=0)
+    button_forward.grid(row=1, column=2)
 
-button_0.grid(row=4, column=0)
-button_clear.grid(row=4, column=1, columnspan=2)
-button_addition.grid(row=5, column=0)
-button_equal.grid(row=5, column=1, columnspan=2)
+
+
+def back():
+    global my_label
+    global button_forward
+    global button_back
+
+
+button_back = Button(root, text="<<", command=lambda: back())
+button_forward  = Button(root, text=">>", command=lambda: forward(2))
+button_quit = Button(root, text=f"Exit Program", command=root.quit)
+
+
+button_back.grid(row=1, column=0)
+button_forward.grid(row=1, column=2)
+button_quit.grid(row=1, column=1)
 
 
 root.mainloop()
