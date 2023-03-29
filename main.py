d55d85d42bf32f42eba35f12889ce4fe8a6727b7
foxx <95589721+Fgfox9@users.noreply.github.com>
2023-03-29 07:24:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -23,20 +23,33 @@ def forward(image_number):
     my_label = Label(image=image_list[image_number-1])
     button_forward = Button(root, text=">>", command=lambda: forward(image_number+1))
     button_back = Button(root, text="<<", command=lambda: back(image_number-1))
-    my_label.grid(row=0, column=0, columnspan=3)
+    
+    if image_number == 3:
+        button_forward = Button(root, text=">>", state=DISABLED)
 
+    my_label.grid(row=0, column=0, columnspan=3)
     button_back.grid(row=1, column=0)
     button_forward.grid(row=1, column=2)
 
 
 
-def back():
+def back(image_number):
     global my_label
     global button_forward
     global button_back
+    my_label.grid_forget()
+    my_label = Label(image=image_list[image_number-1])
+    button_forward = Button(root, text=">>", command=lambda: forward(image_number+1))
+    button_back = Button(root, text="<<", command=lambda: back(image_number-1))
+    
+    if image_number == 1:
+        button_back = Button(root, text="<<", state=DISABLED)
+        
+    my_label.grid(row=0, column=0, columnspan=3)
+    button_back.grid(row=1, column=0)
+    button_forward.grid(row=1, column=2)
 
-
-button_back = Button(root, text="<<", command=lambda: back())
+button_back = Button(root, text="<<", command=back, state=DISABLED)
 button_forward  = Button(root, text=">>", command=lambda: forward(2))
 button_quit = Button(root, text=f"Exit Program", command=root.quit)
 
